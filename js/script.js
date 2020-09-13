7ea1043c8fa371d8c98563021b4c9e956efb6a64
Precious Solomon <preciouskayili@gmail.com>
2020-09-13 18:12:43 +0100

    New UI design project for an interior design website

--- js/script.js
+++ js/script.js
@@ -1,64 +1,14 @@
-var countries = [
-  "Ghana",
-  "USA",
-  "London",
-  "UK",
-  "Cameron",
-  "Dubai",
-  "Nigeria",
-  "Turkey",
-  "Canada",
-];
-function populateSelect() {
-  var html = "";
-  html += "<option disabled>-- Choose country --</option>";
-  for (let index = 0; index < countries.length; index++) {
-    const element = countries[index];
-    html += "<option value='" + element + "'>" + element + "</option>";
-  }
-
-  $("#from").html(html);
-}
-
-function populateSecond() {
-  var fromValue = document.getElementById("from").value;
-  var html = "";
-  html += "<option disabled>-- Choose country --</option>";
-  for (let index = 0; index < countries.length; index++) {
-    const element = countries[index];
-    if (fromValue != element) {
-      html += "<option value='" + element + "'>" + element + "</option>";
-    }
-  }
-  $("#to").html(html);
-}
-
-function deleteItem() {
-  var html = "";
-
-  html -= "<div></div>";
-
-  $("#delete").html(html);
-}
-
-$(document).ready(function () {
-  $("#deleteThis").click(function () {
-    $("#deleteEmptyResult").css("display", "none");
-  });
+// Animations init
+new WOW().init();
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function() {
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function() {
+            $("." + selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
 });
-
-function deleteCard() {
-  var html = "";
-
-  html -= "<div></div>";
-
-  $("#delete").html(html);
-}
-
-function deleteInput() {
-  document.getElementById("deleteItem").disabled = true;
-}
-
-function restoreInput() {
-  document.getElementById("deleteItem").disabled = false;
-}
